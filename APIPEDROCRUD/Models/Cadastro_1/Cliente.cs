7bc559903c547464085a68927399f9243771521e
Pedro <pedrogabrielcruzlima@gmail.com>
2023-03-21 04:04:57 -0300

    adicionar Pastas

---
+++ APIPEDROCRUD/Models/Cadastro_1/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIPEDROCRUD.Models;
+
+public partial class Cliente
+{
+    public int ID { get; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Endereco { get; set; }
+
+    public string Cep { get; set; }
+
+    public string Bairro { get; set; }
+
+    public string Cidade { get; set; }
+
+    public string Uf { get; set; }
+
+    public string Telefone { get; set; } = null!;
+}
