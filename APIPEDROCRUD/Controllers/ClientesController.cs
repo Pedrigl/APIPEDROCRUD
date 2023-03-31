612610c8614556ed3fa9cff391aaae6da1ed42dd
Pedro <pedrogabrielcruzlima@gmail.com>
2023-03-31 02:22:22 -0300

    fix post

--- APIPEDROCRUD/Controllers/ClientesController.cs
+++ APIPEDROCRUD/Controllers/ClientesController.cs
@@ -32,15 +32,24 @@ namespace APIPEDROCRUD.Controllers
 
         public async Task<IActionResult> Post(Cliente cliente)
         {
-            if (int.Parse(cliente.Telefone.Substring(2)) != 9)
-                return BadRequest("Telefones precisam ter DDD e o primeiro numero tem que ser 9 \r\n Exemplo:xx9xxxxxxxx");
-            if (cliente.Uf.Length != 2)
-                return BadRequest("A Uf precisa ter 2 dígitos");
-            if (cliente.Cep.Length != 8)
-                return BadRequest("o CEP precisa ter 8 dígitos");
-            _context.Add(cliente);
+            try
+            {
+                if (int.Parse(cliente.Telefone.Substring(2)) != 9)
+                    return BadRequest("Telefones precisam ter DDD e o primeiro numero tem que ser 9 \r\n Exemplo:xx9xxxxxxxx");
+                if (cliente.Uf.Length != 2)
+                    return BadRequest("A Uf precisa ter 2 dígitos");
+                if (cliente.Cep.Length != 8)
+                    return BadRequest("o CEP precisa ter 8 dígitos");
 
-            await _context.SaveChangesAsync();
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+                throw;
+            }
+
             return Ok(cliente);
         }
 
